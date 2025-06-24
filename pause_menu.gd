480ca53553e98a59ccbd2a1eb9433afbf1b5ff5d
Time-Paradox23 <blabro36@kingshigh.school.nz>
2025-06-25 09:40:34 +1200

    Pause menu

---
+++ pause_menu.gd
@@ -0,0 +1,14 @@
+extends Control
+
+@onready var main = $"../../"
+
+func _on_resume_pressed() -> void:
+	main.pauseMenu()
+
+
+func _on_settings_pressed() -> void:
+	print("Settings pressed.")
+
+
+func _on_quit_pressed() -> void:
+	get_tree().quit()
