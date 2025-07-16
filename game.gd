f42438d0038300af99cdbdc0e3ebb1cd060652d9
Time-Paradox23 <blabro36@kingshigh.school.nz>
2025-07-17 09:59:15 +1200

    Pause menu now hides

--- game.gd
+++ game.gd
@@ -3,6 +3,11 @@ extends Node3D
 @onready var pause_menu = $"Player/Pause menu"
 var paused = false
 
+func _ready():
+	pause_menu.hide()
+	Engine.time_scale = 1 
+	Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_just_pressed("pause"):
@@ -12,9 +17,9 @@ func pauseMenu():
 	if paused: 
 		pause_menu.hide()
 		Engine.time_scale = 1 
-		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		Input.mouse_mode = Input.MOUSE_MODE_HIDDEN
 	else:
 		pause_menu.show()
 		Engine.time_scale = 0
-	
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 	paused = !paused
--- pause_menu.gd
+++ pause_menu.gd
@@ -2,13 +2,15 @@ extends Control
 
 @onready var main = $"../../"
 
+func _ready():
+	main.pauseMenu()
 
 func _on_resume_pressed() -> void:
 	main.pauseMenu()
 
 
 func _on_settings_pressed() -> void:
-	print("Settings pressed.")
+	get_tree()
 
 
 func _on_quit_pressed() -> void:
