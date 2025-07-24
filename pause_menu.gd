5505ca48d828abac0f8ff21f790a51596262fbcb
Time-Paradox23 <blabro36@kingshigh.school.nz>
2025-07-25 10:00:11 +1200

    Updated pause menu

--- pause_menu.gd
+++ pause_menu.gd
@@ -1,17 +1,34 @@
 extends Control
 
-@onready var main = $"../../"
 
-func _ready():
-	main.pauseMenu()
+func _ready() -> void:
+	visible = false
+	# Optional: Connect signals programmatically
+	$Resume.pressed.connect(_on_resume_pressed)
+	$Settings.pressed.connect(_on_settings_pressed)
+	$Exit.pressed.connect(_on_exit_pressed)
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_cancel"):
+		if get_tree().paused:
+			resume_game()
+		else:
+			pause_game()
+
+func pause_game() -> void:
+	get_tree().paused = true
+	visible = true
+
+func resume_game() -> void:
+	get_tree().paused = false
+	visible = false
 
 func _on_resume_pressed() -> void:
-	main.pauseMenu()
+	resume_game()
 
+func _on_exit_pressed() -> void:
+	get_tree().quit()
 
 func _on_settings_pressed() -> void:
-	get_tree()
-
-
-func _on_quit_pressed() -> void:
-	get_tree().quit()
+	# Example: Show settings submenu
+	$SettingsSubMenu.visible = true
